c380a8e67e0adaddc14bc3af3354e769682cf0f4
Matheus Hygino <matheusghygino@gmail.com>
2025-01-26 23:06:43 -0300

    calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    background-color:rgb(112, 52, 240);
+    width: 100%;
+    height: 80px;
+    display: flex;
+    flex-grow: 1;
+    input {
+        width: 100%;
+        height: 100%;
+        color: #fff;
+        padding: 0 5px;
+        font-size: 18px;
+        font-weight: 700;
+    }
+`
